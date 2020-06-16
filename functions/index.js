b67fd323348477f4b397a41ec24b2bfd557e67ae
Pradyumn Singh <spradyumn79@gmail.com>
2020-06-16 12:31:45 +0530

    [ADD]:image upload

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
 const functions = require('firebase-functions');
 const express = require('express');
 
+app = express();
+
 const {
     signup,
     login,
     addUserDetails,
     getAuthenticatedUser,
     getUserDetails,
+    uploadImage
 } = require('./handlers/users');
 
 app.post('/signup', signup);
@@ -14,5 +17,6 @@ app.post('/login', login);
 app.post('/user', addUserDetails);
 app.get('/user/:handle', getUserDetails);
 app.get('/user', getAuthenticatedUser);
+app.post('/user/image', uploadImage);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
