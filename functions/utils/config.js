6c337b271b17fc2d44f6bb61e15dde102a5ea98e
Shalini1512 <56508191+Shalini1512@users.noreply.github.com>
2020-10-31 18:01:48 +0530

    Update config.js

--- functions/utils/config.js
+++ functions/utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    apiKey: "AIzaSyD43v871x3P67UF41QR6lRU2vXXYVKQwm0",
-    authDomain: "social-backend-3c9e5.firebaseapp.com",
-    databaseURL: "https://social-backend-3c9e5.firebaseio.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.DOMAIN,
+    databaseURL: process.env.DB_URL
     projectId: "social-backend-3c9e5",
     storageBucket: "social-backend-3c9e5.appspot.com",
     messagingSenderId: "911153537270",
