75d4b7afc2c3a32bc33736cd88ce4d542b76d5d7
Shalini1512 <56508191+Shalini1512@users.noreply.github.com>
2020-10-31 18:05:26 +0530

    Update validators.js

--- functions/utils/validators.js
+++ functions/utils/validators.js
@@ -1,12 +1,10 @@
 const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (email.match(regEx)) return true;
-    else return false;
+    return email.match(regEx)
 };
 
 const isEmpty = (string) => {
-    if (string.trim() === '') return true;
-    else return false;
+    return string.trim() === '');
 };
 
 exports.validateSignupData = (data) => {
